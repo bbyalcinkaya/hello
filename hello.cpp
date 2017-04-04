27b080b11297f2e0d0ce9b511616521305040a97
Burak Bilge Yalçınkaya <bbyalcinkaya@gmail.com>
2017-04-05 01:00:38 +0300

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,10 +1,6 @@
 #include <iostream>
-#include <string>
-using namespace std;
+
 int main(){
-  string s = "Hello world!\n";
-  for(auto i: s)
-    cout << i;
-  
+  std::cout << "Hello world!\n";
   return 0;
 }
