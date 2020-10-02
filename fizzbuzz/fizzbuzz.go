b4af7332075714b29963cf72a4777a968e8f5e97
Uğur Uysal <uguruysal0@users.noreply.github.com>
2020-10-02 09:53:41 +0300

    Fix fizzbuzz errör
    Fix fizzbuzz errör

--- fizzbuzz/fizzbuzz.go
+++ fizzbuzz/fizzbuzz.go
@@ -10,10 +10,14 @@ func main() {
 
 func FizzBuzz(i int) {
 	if i % 3 == 0 {
-		fmt.Printf("Fizz\n")
-	} else if i % 5 == 0 {
-		fmt.Printf("Buzz\n")
-	} else {
-		fmt.Printf("number=%d\n", i)
+		fmt.Printf("Fizz")
 	}
+	if i % 5 == 0 {
+		fmt.Printf("Buzz")
+	}
+	if i%5!=0 && i%3!=0	{
+		fmt.Printf("number=%d", i)
+	}
+	fmt.Printf("\n")
 }
+
